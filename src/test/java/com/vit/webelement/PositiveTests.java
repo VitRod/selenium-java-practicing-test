bfb6f539025b00e129df5211365b548611fb442a
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-20 06:03:40 +0300

    added test with web element

--- src/test/java/com/vit/webelement/PositiveTests.java
+++ src/test/java/com/vit/webelement/PositiveTests.java
@@ -1,18 +1,17 @@
-package com.vit;
+package com.vit.webelement;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginPositiveTest {
+public class PositiveTests {
 
 	@Test
 	public void loginTest() {
 		System.out.println("Starting loginTest");
-
+		
 //		Create driver
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -27,42 +26,32 @@ public class LoginPositiveTest {
 		String url = "http://the-internet.herokuapp.com/login";
 		driver.get(url);
 		System.out.println("Page is opened.");
+		
+		// sleep for 2 seconds
+		sleep(2000);
 
 //		enter username
 		WebElement username = driver.findElement(By.id("username"));
-		username.sendKeys("tomsmith");
 
 //		enter password
 		WebElement password = driver.findElement(By.name("password"));
-		password.sendKeys("SuperSecretPassword!");
-
+		
 //		click login button
 		WebElement logInButton = driver.findElement(By.tagName("button"));
-		logInButton.click();
 
-		sleep(3000);
-
-//		verifications:
+//		verificatins:
 //		 new url
-		String expectedUrl = "http://the-internet.herokuapp.com/secure";
-		String actualUrl = driver.getCurrentUrl();
-		Assert.assertEquals(actualUrl, expectedUrl, "Actual page url is not the same as expected");
-
+		
 //		 logout button is visible
 		WebElement logOutButton = driver.findElement(By.xpath("//a[@class='button secondary radius']"));
-		Assert.assertTrue(logOutButton.isDisplayed(), "Log Out button is not visible");
-
+		
+		
 //		 succesful login message
-		// WebElement successMessage = driver.findElement(By.cssSelector("#flash"));
-		WebElement successMessage = driver.findElement(By.xpath("//div[@id='flash']"));
-		String expectedMessage = "You logged into a secure area!";
-		String actualMessage = successMessage.getText();
-		// Assert.assertEquals(actualMessage, expectedMessage, "Actual message is not
-		// the same as expected");
-		Assert.assertTrue(actualMessage.contains(expectedMessage),
-				"Actual message does not contain expected message.\nActual Message: " + actualMessage
-						+ "\nExpected Message: " + expectedMessage);
-
+		WebElement successMessage = driver.findElement(By.cssSelector("#flash"));
+		
+		
+		
+		
 		// Close browser
 		driver.quit();
 	}
